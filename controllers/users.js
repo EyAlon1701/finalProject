35235f4e180e691cf5c4fdc33e152709236d4ef1
EyAlon1701 <eyalon022@gmail.com>
2022-06-23 21:48:06 +0300

    fix

--- controllers/users.js
+++ controllers/users.js
@@ -5,45 +5,6 @@ const User = require('../models/users')
 
 const router = express.Router();
 
-router.get('/login', (request,response) => {
-
-    User.findAll()
-    .then(users => {
-        response.render('login', {
-            data: users
-        })
-    })
-    .catch(err => {
-        console.log(err);
-    }) 
-});
-
-router.get('/register', (request,response) => {
-
-    User.findAll()
-    .then(users => {
-        response.render('register', {
-            data: users
-        })
-    })
-    .catch(err => {
-        console.log(err);
-    }) 
-});
-
-router.get('/', (request,response) => {
-
-    User.findAll()
-    .then(users => {
-        response.render('homePage', {
-            data: users
-        })
-    })
-    .catch(err => {
-        console.log(err);
-    }) 
-});
-
 router.post('/login', async(request,response) =>{
 
     const {email,password} = request.body;
@@ -70,7 +31,6 @@ router.post('/login', async(request,response) =>{
                     console.log(isMatch);
                     if(isMatch){
                         const token = jsonwebtoken.sign(user[0].email, 'EyAlon#1701');
-                        //  response.redirect('storeHomePage');
                         return response.status(200).json({
                             message: token
                         })
@@ -109,6 +69,7 @@ router.post('/register', async(request,response) =>{
             message: 'Please fill inputs'
         });
     }
+
     const hash_password = await bcryptjs.hash(password,10);
     User.create({
         firstName: firstName,
@@ -123,11 +84,93 @@ router.post('/register', async(request,response) =>{
         return response.status(200).json({
             message: "succeed! now login"
         })
-       // response.redirect('/storeHomePage');
     })
     .catch(err => {
         return response.status(500).json({
             message: 'ERROR'
+        });
+    })
+})
+
+router.post('/verify', async(request,response) =>{
+
+    const {email,passcode} = request.body;
+
+    if(!email || !passcode){
+
+        return response.status(200).json({
+            message: 'Please fill inputs'
+        })
+    }
+    User.findAll({where: {email: email}})
+    .then(user => {
+        if(user[0]==null)
+        {   
+            return response.status(500).json({
+                message: 'User Not found'
+            })  
+        }
+        else
+        {
+            if(user[0].passcode == passcode)
+            {
+                user[0].isApproved = true;
+                user[0].save();
+                return response.status(200).json({
+                    message: 'The account is now verified!'
+                })
+            }
+            else 
+            {
+                return response.status(200).json({
+                    message: 'The code is incorrect'
+                })
+            }
+        }
+    })
+    .catch(err => {
+        return response.status(500).json({
+            message: err
+        })
+    })
+})
+
+router.post('/forgetPassword', async (request, response) => {
+
+  
+})
+
+router.post('/updatePassword', async (request, response) => {
+
+      const {email,password} = request.body;
+
+    if(!email || !password){
+
+        return response.status(200).json({
+            message: 'Please fill inputs'
+        })
+    }
+
+    Account.findAll({where: {email: email}})
+    .then(user => {
+        if(user[0] == null)
+        {
+            return response.status(200).json({
+                message: 'User Not found'
+            })
+        }
+        else {
+            const hash_password = await bcryptjs.hash(password,10);
+            user[0].password = hash_password;
+            user[0].save();
+            return response.status(200).json({
+                message:  'Password updates successfully'
+            })
+        }
+    }) 
+    .catch(err => {
+        return response.status(500).json({
+            message: err
         });
     })
 })
--- index.js
+++ index.js
@@ -5,9 +5,6 @@ const path = require('path');
 
 const app = express();
 
-app.set('view engine','ejs');
-app.set('views','views');
-
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
 
